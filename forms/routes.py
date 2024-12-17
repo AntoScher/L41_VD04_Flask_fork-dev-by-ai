d009cc4ceeb343a2fee8da781eb324e2401c3676
AntoScher <asc8er@gmail.com>
2024-12-17 11:59:30 +0300

    pre final

--- forms/routes.py
+++ forms/routes.py
@@ -1,27 +1,19 @@
-from  flask import Flask, render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for
 from forms import app
+from forms.forms import PostForm
 
 posts = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        title = request.form.get("title")
-        content = request.form.get("content")
-        content1 = request.form.get("content1")
-        content2 = request.form.get("content2")
-        post = {"title": title, "content": content,"content1": content1,"content2": content2}
-        # создаёт условие для проверки наличия данных в полях title и content
-        if title and content:
-            posts.append({
-            'title': title,
-            'content': content,
-            'content1': content1,
-            'content2': content2
-            })
-        # использует для обновления страницы и предотвращения повторной отправки формы.
-        # возвращает отрендеренный шаблон с переданными данными постов
+    form = PostForm()
+    if form.validate_on_submit():
+        post = {
+            "title": form.title.data,
+            "content": form.content.data,
+            "content1": form.content1.data,
+            "content2": form.content2.data
+        }
+        posts.append(post)
         return redirect(url_for("index"))
-    return render_template("blog.html", posts=posts)
-
-
+    return render_template("form.html", form=form, posts=posts)
